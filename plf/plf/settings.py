ef4789c47ed6e913603a1df8b2a2464d124e1adb
Matt Goldsmith <matthewjg@mac.com>
2013-04-04 03:15:33 -0400

    Revert "home page, etc"
    This reverts commit 0581832af07027d764f6ee72d53e768204d4dc1d.

--- plf/plf/settings.py
+++ plf/plf/settings.py
@@ -111,6 +111,7 @@ WSGI_APPLICATION = 'plf.wsgi.application'
 TEMPLATE_DIRS = (
     '/Users/samlichtenberg/Documents/cs333/PLF/plf/plf/templates',
     '/Users/tortorareed/Desktop/HW/cos333/PLF/plf/plf/templates',
+    '/Users/matthew/Desktop/code/github/plf/plf/plf/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
