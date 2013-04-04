5be26a7b68415d5b19a743bcd4c959ffc576f09a
Reed <tortorareed@gmail.com>
2013-04-04 01:58:57 -0400

    Added Reed's template folder
    Also changed timezone to new york

--- plf/plf/settings.py
+++ plf/plf/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/New_York'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -110,6 +110,7 @@ WSGI_APPLICATION = 'plf.wsgi.application'
 # change this to your own template directory (comment mine out plz)
 TEMPLATE_DIRS = (
     '/Users/samlichtenberg/Documents/cs333/PLF/plf/plf/templates',
+    '/Users/tortorareed/Desktop/HW/cos333/PLF/plf/plf/templates',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
