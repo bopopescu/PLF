caabbd2791aa2225f2618604605f099a96ddfcc2
splichte <splichte@princeton.edu>
2013-04-09 00:39:14 -0400

    trying

--- plf/info/models.py
+++ plf/info/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    items = models.ManyToManyField('Item')
+
 class Item(models.Model):
     status = models.BooleanField()
     category = models.CharField(max_length=100)
-#    user = models.ForeignKey(User)
+    student = models.ForeignKey('User')
     desc = models.CharField(max_length=250)
     sub_date = models.DateField(null=True)
     event_date = models.DateField(null=True)
     location = models.CharField(max_length=100)
-    # picture = models.ImageField(upload_to='pics')
+    picture = models.ImageField(upload_to='pics', null=True)
 
     def __unicode__(self):
         return u'%s' % (self.category)
 
-#class User(models.Model):
+
