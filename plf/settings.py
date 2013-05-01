ca56a65630e40eae9baf17768a3a50aaae64c4bd
Matt Goldsmith <matthewjg@mac.com>
2013-05-01 03:53:03 -0400

    settings

--- plf/settings.py
+++ plf/settings.py
@@ -64,7 +64,7 @@ MEDIA_URL = '/media/'
 
 import os
 
-PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
