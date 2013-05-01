b69075681acf9f77ecef4959224924c01ffa83f3
Matt Goldsmith <matthewjg@mac.com>
2013-05-01 04:01:57 -0400

    this is annoying

--- plf/settings.py
+++ plf/settings.py
@@ -70,7 +70,7 @@ PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_DIR,'static/')
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
