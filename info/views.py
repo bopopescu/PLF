a940f101caef8758dae68166b5a0cfbf2cf20a65
Reed <tortorareed@gmail.com>
2013-05-01 22:11:49 -0400

    More CSS and layout

--- info/views.py
+++ info/views.py
@@ -90,6 +90,10 @@ def home(request):
 
     context['items'] = items
     context['error'] = error
+    if 'auth' not in request.session:
+        context['logged_in'] = False
+    else:
+        context['logged_in'] = True
     return render_to_response('home.html', context)
 
 def dataReturn(request):
