6022bbd5e1e35765ae6a3a2f38f56025ba70ab9e
splichte <splichte@princeton.edu>
2013-05-06 17:25:47 -0400

    html5 form

--- info/forms.py
+++ info/forms.py
@@ -4,6 +4,7 @@ from django.contrib.admin.widgets import AdminDateWidget
 from django.forms import extras
 import PIL
 import datetime
+import html5.forms.widgets as html5_widgets
 
 STATUS=[('Lost', 'Lost'), ('Found', 'Found')]
 CATEGORIES = (('Clothing', 'Clothing'), ('Wallet/Keys/Prox', 'Wallet/Keys/Prox'), ('Phone', 'Phone'), ('Charger', 'Charger'), ('Electronics', 'Electronics'), ('Accessories', 'Accessories'), ('Textbook', 'Textbook'), ('Backpack', 'Backpack'), ('Other', 'Other'), ('Black North Face', 'Black North Face'))
@@ -14,8 +15,9 @@ class SubmitForm(forms.Form):
     status = forms.ChoiceField(choices=STATUS, widget=forms.RadioSelect())
     category = forms.ChoiceField(choices=CATEGORIES)
     location = forms.CharField(required=False, max_length=100)
-    event_date = forms.DateField(initial=now, widget=extras.SelectDateWidget(years=range(2012, datetime.date.today().year + 1)), required=False)
-    desc = forms.CharField(widget=forms.Textarea(attrs={'rows':'4', 'cols':'40'}), max_length=250)
+    #event_date = forms.DateField(initial=now, widget=extras.SelectDateWidget(years=range(2012, datetime.date.today().year + 1)), required=False)
+    event_date = forms.DateField(widget=html5_widgets.DateInput)
+    desc = forms.CharField(widget=forms.Textarea(attrs={'rows':'2', 'cols':'40'}), max_length=250)
     picture = forms.ImageField(required=False)
     name = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}), max_length=20)
     #netid = forms.CharField()
