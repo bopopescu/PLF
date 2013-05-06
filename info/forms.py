e28e99710fd9860c37f109bf868f400204370a25
splichte <splichte@princeton.edu>
2013-05-06 16:48:36 -0400

    fixed input

--- info/forms.py
+++ info/forms.py
@@ -13,11 +13,11 @@ now = datetime.datetime.today()
 class SubmitForm(forms.Form):
     status = forms.ChoiceField(choices=STATUS, widget=forms.RadioSelect())
     category = forms.ChoiceField(choices=CATEGORIES)
-    location = forms.CharField(required=False)
+    location = forms.CharField(required=False, max_length=100)
     event_date = forms.DateField(initial=now, widget=extras.SelectDateWidget(years=range(2012, datetime.date.today().year + 1)), required=False)
-    desc = forms.CharField(widget=forms.TextInput(attrs={'size':'100'}))
+    desc = forms.CharField(widget=forms.Textarea(attrs={'size':'250'}), max_length=250)
     picture = forms.ImageField(required=False)
-    name = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}))
+    name = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}), max_length=20)
     #netid = forms.CharField()
 
 	#def clean_message(self):
