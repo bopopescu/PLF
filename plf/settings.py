090af6ac86b94021f2610aa18f4589db52d94282
Matt Goldsmith <matthewjg@mac.com>
2013-05-07 02:56:52 -0400

    please work

--- plf/settings.py
+++ plf/settings.py
@@ -99,8 +99,7 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3.S3Storage'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'vx2f!p3@g+%-k2-b1$!wtu25kl0^b_yi2s7@g19wvpxv-%!m#r'
