201f7e7b7240619eb39b6faf1d2924b5bce34c35
Matt Goldsmith <matthewjg@mac.com>
2013-05-07 16:04:30 -0400

    django-resized

--- plf/settings.py
+++ plf/settings.py
@@ -150,6 +150,7 @@ INSTALLED_APPS = (
     'plf',
     'south',
     'storages',
+    'django-resized',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
