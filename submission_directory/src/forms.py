0470800b47a7ef599bdbcd1c67c6e8538d996980
splichte <splichte@princeton.edu>
2013-05-14 15:59:12 -0400

    updated direc

---
+++ submission_directory/src/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.forms.fields import DateField
+from django.contrib.admin.widgets import AdminDateWidget 
+from django.forms import extras
+import PIL
+import datetime
+import html5.forms.widgets as html5_widgets
+
+STATUS=[('Lost', 'Lost'), ('Found', 'Found')]
+CATEGORIES = (('Clothing', 'Clothing'), ('Wallet/Keys/Prox', 'Wallet/Keys/Prox'), ('Phone', 'Phone'), ('Charger', 'Charger'), ('Electronics', 'Electronics'), ('Accessories', 'Accessories'), ('Textbook', 'Textbook'), ('Backpack', 'Backpack'), ('Black North Face', 'Black North Face'), ('Other', 'Other'))
+
+now = datetime.datetime.today()
+
+class SubmitForm(forms.Form):
+    status = forms.ChoiceField(choices=STATUS, widget=forms.RadioSelect())
+    category = forms.ChoiceField(choices=CATEGORIES)
+    location = forms.CharField(required=False, max_length=100)
+    #event_date = forms.DateField(initial=now, widget=extras.SelectDateWidget(years=range(2012, datetime.date.today().year + 1)), required=False)
+    event_date = forms.DateField(widget=html5_widgets.DateInput, required=False)
+    desc = forms.CharField(widget=forms.Textarea(attrs={'rows':'2', 'cols':'40','maxlength':250}), max_length=250)
+    picture = forms.ImageField(required=False)
+    name = forms.CharField(widget=forms.TextInput(attrs={'size':'20'}), max_length=20)
+    #netid = forms.CharField()
+
+	#def clean_message(self):
+    #    message = self.cleaned_data['message']
+    #    num_words = len(message.split())
+    #    if num_words < 4:
+    #        raise forms.ValidationError("Not enough words!")
+    #    return message
